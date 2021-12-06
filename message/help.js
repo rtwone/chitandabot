79dcc94a0ca15c70e1224320e550dc58f85c3674
Yanzヤ` <78160909+rtwone@users.noreply.github.com>
2021-12-06 12:54:47 +0000

    Update help.js

--- message/help.js
+++ message/help.js
@@ -1,10 +1,16 @@
 exports.allmenu = (conn, prefix, pushname) => {
     return `Hi ${pushname ? pushname : 'Kak'}, Berikut list menu yang tersedia di Bot ini!
 
+*More Features*
 ≻ ${prefix}menu
 ≻ ${prefix}donasi
 ≻ ${prefix}sticker
+
+*Downloader*
 ≻ ${prefix}tiktok
+≻ ${prefix}ytmp4
+≻ ${prefix}ytmp3
+≻ ${prefix}instagram
 
 fitur lainnya masih dalam tahap pengembangan, request fitur chat ${prefix}owner
     `
