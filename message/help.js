242371b2cd704a789e35b74db78c8380d9fdec43
Yanzヤ` <78160909+rtwone@users.noreply.github.com>
2021-12-20 01:49:17 +0000

    Add files via upload

--- message/help.js
+++ message/help.js
@@ -1,24 +1,103 @@
-exports.allmenu = (conn, prefix, pushname) => {
-    return `Hi ${pushname ? pushname : 'Kak'}, Berikut list menu yang tersedia di Bot ini!
+const moment = require("moment-timezone");
+const fs = require("fs");
 
-*Information Bot*
-≻ ${prefix}menu
-≻ ${prefix}donasi
-≻ ${prefix}speed
-≻ ${prefix}runtime
+moment.tz.setDefault("Asia/Jakarta").locale("id");
 
-*Converter/Tools*
-≻ ${prefix}sticker
-≻ ${prefix}toimg
-≻ ${prefix}tovid
+let dt = moment(Date.now()).tz('Asia/Jakarta').locale('id').format('a')
+const ucapanWaktu = "Selamat "+dt.charAt(0).toUpperCase() + dt.slice(1)
+let setting = JSON.parse(fs.readFileSync('./config.json'))
+const { getLimit, getBalance, cekGLimit } = require("../lib/limit")
 
-*Downloader*
-≻ ${prefix}tiktok
-≻ ${prefix}ytmp4
-≻ ${prefix}ytmp3
-≻ ${prefix}instagram
-≻ ${prefix}facebook
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001)
 
-fitur lainnya masih dalam tahap pengembangan, request fitur chat ${prefix}owner
-    `
+function toCommas(x) {
+	x = x.toString()
+	var pattern = /(-?\d+)(\d{3})/;
+     while (pattern.test(x))
+	   x = x.replace(pattern, "$1,$2");
+	return x;
+}
+
+exports.allmenu = (sender, prefix, pushname, isOwner, isPremium, balance, limit, limitCount, glimit, gcount) => {
+	return `*── 「 ${setting.botName} - MD Beta 」 ──*
+	
+  _*${ucapanWaktu} ${pushname !== undefined ? pushname : 'Kak'}*_
+
+    Library : *Baileys-MD*.
+    Prefix : ( ${prefix} )
+    Tanggal Server : ${moment.tz('Asia/Jakarta').format('DD/MM/YY')}
+    Waktu Server : ${moment.tz('Asia/Jakarta').format('HH:mm:ss')}
+
+	Status : ${isOwner ? 'Owner' : isPremium ? 'Premium' : 'Free'}
+	Limit Harian : ${isOwner ? '-' : isPremium ? 'Unlimited' : getLimit(sender, limitCount, limit)}
+	Limit Game : ${isOwner ? '-' : cekGLimit(sender, gcount, glimit)}
+	Balance : $${toCommas(getBalance(sender, balance))}
+
+  *( 📍 )  Main Menu*
+  ≻ ${prefix}menu
+  ≻ ${prefix}owner
+  ≻ ${prefix}donasi
+  ≻ ${prefix}speed
+  ≻ ${prefix}runtime
+  ≻ ${prefix}cekprem
+  ≻ ${prefix}listprem
+
+  *( ✏️ )  Converter/Tools*
+  ≻ ${prefix}sticker
+  ≻ ${prefix}toimg
+  ≻ ${prefix}tovid
+
+  *( ⌛ )  Downloader*
+  ≻ ${prefix}play
+  ≻ ${prefix}tiktok
+  ≻ ${prefix}ytmp4
+  ≻ ${prefix}ytmp3
+  ≻ ${prefix}getvideo
+  ≻ ${prefix}getmusic
+  ≻ ${prefix}instagram
+  ≻ ${prefix}facebook
+  
+  *( ♻️ )  Random Menu*
+  ≻ ${prefix}quote
+  ≻ ${prefix}cecan
+  ≻ ${prefix}cogan
+  
+  *( 🔎 )  Search Menu*
+  ≻ ${prefix}lirik
+  ≻ ${prefix}grupwa
+  ≻ ${prefix}ytsearch
+  
+  *( 🎮 )  Game Menu*
+  ≻ ${prefix}tictactoe
+  ≻ ${prefix}delttc
+  ≻ ${prefix}tebakgambar
+  
+  *( 🏦 )  Payment & Bank*
+  ≻ ${prefix}buylimit
+  ≻ ${prefix}buyglimit
+  ≻ ${prefix}transfer
+  ≻ ${prefix}limit
+  ≻ ${prefix}balance
+  
+  *( 👥 )  Group Menu*
+  ≻ ${prefix}linkgrup
+  ≻ ${prefix}setppgrup
+  ≻ ${prefix}setnamegc
+  ≻ ${prefix}setdesc
+  ≻ ${prefix}group
+  ≻ ${prefix}revoke
+  ≻ ${prefix}hidetag
+  
+  *( 🧑🏻‍💻 )  Owner Menu*
+  > evalcode
+  x evalcode-2
+  $ executor
+  ≻ ${prefix}setppbot
+  ≻ ${prefix}exif
+  ≻ ${prefix}leave
+  ≻ ${prefix}addprem
+  ≻ ${prefix}delprem
+
+`+'     \`\`\`Powered by Nodejs\`\`\`'
 }
