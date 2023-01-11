c361638c1b885a7c51081c5630ab789125fc1631
Irfan Hariyanto <78160909+rtwone@users.noreply.github.com>
2023-01-11 19:52:15 +0700

    Update msg.js

--- message/msg.js
+++ message/msg.js
@@ -1,6 +1,6 @@
 /**
   * Created by Irfan
-  * Contact me on WhatsApp wa.me/62877877772777
+  * Contact me on WhatsApp wa.me/6285175222272
   * Follow me on Instagram @irfann._x
   * If you want to buy an updated script that is not encrypted, please WhatsApp me
 */
