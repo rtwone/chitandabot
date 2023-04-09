a5e46106e3dcbc99221d7de7e664ceb51bf776ae
Irfan H <78160909+rtwone@users.noreply.github.com>
2023-04-09 18:33:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
   * Edit features in './message/msg.js'
-  * Contact me on WhatsApp wa.me/6285175222272
+  * Contact me on WhatsApp wa.me/628988808885
   * Follow https://github.com/rtwone
   * If you want to buy an updated script that is not encrypted, please WhatsApp me
 */
